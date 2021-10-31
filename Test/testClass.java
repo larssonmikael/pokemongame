c3f7740ec54e520a9b44fb1229604cf79a2749f3
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-10-31 12:31:26 +0100

    lagt till lista på tester jag kan köra

--- Test/testClass.java
+++ Test/testClass.java
@@ -1,3 +1,9 @@
 public class testClass {
 
+    // TODO: 2021-10-31 testa att läs skylt där en skylt inte finns
+    // TODO: 2021-10-31 testa att gå till roadblocken och bli tillbakastudsad
+    // TODO: 2021-10-31 testa att gå utanför kartan och bli tillbakastudsad
+    // TODO: 2021-10-31 testa kommandot pet på min pokemon och kolla att hp ökar med 1
+    // TODO: 2021-10-31 testa att läsa in stats på din pokemon när du har den
+
 }
