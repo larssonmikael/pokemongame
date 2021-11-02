664e69fb1084deccdbdcb478cdc1649fa63b68af
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-11-02 23:55:31 +0100

    allt klart. Funkar?

--- Test/testClass.java
+++ Test/testClass.java
@@ -3,11 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class testClass {
+
+//  Test and compare constructors and values
     Pokemon gengar = new Pokemon();
     Pokemon nidorino = new Pokemon();
     Humans mikael = new Humans("Mikael", "That's my name", nidorino, null, true);
     Humans olle = new Humans("Olle", null ,gengar, null, false);
-
 
     @Test
     public void testIsMikaelTrainer() {
@@ -32,14 +33,4 @@ public class testClass {
     public void testComparePokemon() {
         assertNotSame(gengar, mikael.pokemon);
     }
-
-
-    // TODO: 2021-10-31 testa att läs skylt där en skylt inte finns
-    // TODO: 2021-10-31 testa att gå till roadblocken och bli tillbakastudsad
-    // TODO: 2021-10-31 testa att gå utanför kartan och bli tillbakastudsad
-    // TODO: 2021-10-31 testa kommandot pet på min pokemon och kolla att hp ökar med 1
-    // TODO: 2021-10-31 testa att läsa in stats på din pokemon när du har den
-    // TODO: 2021-11-01 nu kan jag bara gå in till rivals place med en pokemon. Testa det
-    // TODO: 2021-11-02 kontrollera att rivalens pokemon är bulbasaur när jag väljer tentacool 
-
 }
