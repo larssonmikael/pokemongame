9594dd1ca5612813871d2ae450b37b4860e719b7
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-11-04 11:41:08 +0100

    tester finns och funkar

--- Test/testClass.java
+++ Test/testClass.java
@@ -1,16 +1,29 @@
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class testClass {
-
 //  Test and compare constructors and values
     Pokemon gengar = new Pokemon();
     Pokemon nidorino = new Pokemon();
     Humans mikael = new Humans("Mikael", "That's my name", nidorino, null, true);
     Humans olle = new Humans("Olle", null ,gengar, null, false);
+    Sign sign = new Sign("a very real message");
+    Location lavender = new Location("Lavender Town", "A hauntingly beautiful town");
 
 
+    @Test
+    public void testLocationDescription() {
+        String result = lavender.getDescription();
+        assertSame(result, "A hauntingly beautiful town");
+    }
+
+    @Test
+    public void testSignMessage() {
+        String message = sign.getMessage();
+        Assertions.assertEquals(message, "a very real message");
+    }
     @Test
     public void testIsMikaelTrainer() {
         boolean trainer = mikael.trainer;
--- src/Sign.java
+++ src/Sign.java
@@ -6,7 +6,7 @@ public class Sign {
         message = inMessage;
     }
 
-    public String getMessage(String message) {
+    public String getMessage() {
         return message;
     }
     public void setMessage(String message) {
